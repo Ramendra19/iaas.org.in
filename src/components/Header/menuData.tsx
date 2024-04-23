7a7cecaa3cdd7b643472862ac421b31454b9ba2a
Ramendra19 <ramendra00singh@gmail.com>
2024-04-23 18:58:47 +0530

    uplode first time website

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,48 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Semiconductor",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Industries",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 5,
+    title: "Events",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 6,
+    title: "Membership",
+    path: "/pricing",
+    newTab: false,
+  },
+  {
+    id:7,
+    title: "Contact",
+    path: "/contact",
+    newTab: false,
+  },
+  
+];
+export default menuData;
